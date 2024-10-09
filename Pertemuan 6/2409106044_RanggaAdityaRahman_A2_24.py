3ad60d5917f619317f836b9aa21198fa764b6303
RxDnz <92702560+RxDnzMC@users.noreply.github.com>
2024-10-09 01:47:45 +0100

    Add files via upload

---
+++ Pertemuan 6/2409106044_RanggaAdityaRahman_A2_24.py
@@ -0,0 +1,135 @@
+# daftar_buku = {
+# "Buku1" : "Harry Potter",
+# "Buku2" : "Percy Jackson",
+# "Buku3" : "Twillight"
+# }
+# print(daftar_buku["Buku1"])
+# print(daftar_buku["Buku2"])
+# print(daftar_buku["Buku3"])
+
+# daftar_buku = {}
+# daftar_buku["Buku1"] = "Harry Potter"
+# daftar_buku["Buku2"] = "Percy Jackson"
+# daftar_buku["Buku3"] = "Twillight"
+# print(daftar_buku)
+
+# Biodata = {
+#     "Nama" : "Aldy Ramadhan Syahputra",
+#     "NIM" : 2109106079,
+#     "KRS" : ["Program Web", "Struktur Data", "Basis Data"],
+#     "Mahasiswa_Aktif" :True,
+#     "Social Media" : {
+#         "Instagram" : "@aldyrmdhns_",
+#         "Discord" : "\'Izanami#6848"
+#     }
+# }
+
+# print(Biodata["Social Media"]["Discord"])
+# print(Biodata["KRS"][1])
+# for i in Biodata.items():
+# print(Biodata.get('KRS'))
+# for i,j in Biodata.items():
+    # print(f"{i} = {j}")
+
+# Nilai = {
+# "Matematika" : 80,
+# "B. Indonesia" : 90,
+# "B. Inggris" : 81,
+# "Kimia" : 78,
+# "Fisika" : 80
+# }
+# print(Nilai)
+
+# Nilai["Sejarah"] = 100
+# Nilai.update({"Biologi" : 100})
+# print(Nilai)
+# del Nilai["Matematika"]
+# print(Nilai)
+# cache = Nilai.pop("Sejarah")
+# print(cache)
+# print(Nilai, len(Nilai))
+# pinjamle = Nilai.copy()
+# print("Pinjam ",pinjamle)
+# print(Nilai.keys())
+# print(Nilai.values())
+
+# key = "apel", "jeruk", "mangga"
+# value = 1
+# buah = dict.fromkeys(key, value)
+# print(buah)
+
+# Nilai = {
+# "Matematika" : 80,
+# "B. Indonesia" : 90,
+# "B. Inggris" : 81
+# }
+# #sebelum Setdefault
+# print(Nilai)
+# print("")
+# #menggunakan setdefault
+# print("Nilai : ", Nilai.setdefault("Kimia", 70))
+# print("")
+# #setelah menggunakan setdefault
+# print(Nilai)
+
+
+# Musik = {
+# "The Chainsmoker" : ["All we Know", "The Paris"],
+# "Alan Walker" : ["Alone", "Lily"],
+# "Neffex" : ["Best of Me", "Memories"]
+# }
+# for i, j in Musik.items():
+#     print(f"Musik milik {i} adalah : ")
+#     for song in j:
+#         print(song)
+#     print("")
+
+
+
+# mahasiswa = {
+# 101 : {"Nama" : "Aldy", "Umur" : 19},
+# 111 : {"Nama" : "Abdul", "Umur" : 18}
+# }
+# for key, value in mahasiswa.items():
+#     print("ID Mahasiswa : ", key)
+#     for key_a, value_a in value.items():
+#         print (key_a, " : ",value_a)
+#     print("")
+
+
+
+# mahasiswa = {
+# 101 : {"Nama" : "Aldy", "Umur" : 19},
+# 111 : {"Nama" : "Abdul", "Umur" : 18}
+# }
+# #Sebelum Dilakukan Perubahan
+# print(mahasiswa)
+
+# #Menambahkan Item pada Nested Dictionary
+# mahasiswa[101]["Angkatan"] = 2023
+# print(mahasiswa)
+
+# #Mengubah Item pada Nested Dictionary
+# mahasiswa[101]["Nama"] = "Rizal"
+# print(mahasiswa)
+
+# #Menghapus Item pada Nested Dictionary
+# del mahasiswa[101]["Umur"]
+# print(mahasiswa)
+
+# for key, value in mahasiswa.items():
+#     print("ID Mahasiswa : ", key)
+#     for key_a, value_a in value.items():
+#         print (key_a, " : ",value_a)
+#     print("")
+
+mahasiswa = {
+    "nama" : "Rangga",
+    "umur" : "18",
+    "NIM" : "2409106044",
+    "jurusan" : "informatika",
+    "angkata" : "2024"
+}
+
+for i,j in mahasiswa.items():
+    print(i,j)
